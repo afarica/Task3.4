b79aa06f05107171637ea443251ed6082362abbd
afarica <57631090+afarica@users.noreply.github.com>
2019-11-22 02:19:37 +0600

    Update task4.py

--- task4.py
+++ task4.py
@@ -1,3 +1,9 @@
+# Minimal positive integer:
+# //Find minimal positive integer that is not in list.
+# Example: [1,2,3,4,6] - Answer 5
+# Example: [1,2,3] - Answer 4
+# Example: [-1, -2, -6] - Answer 1
+# Create effective algorithm
 list1=[-1,-6,-6,6,3,4,6,7,8,9,10]
 t=max(list1)
 list2=set(list(range(min(list1),max(list1)+1)))
